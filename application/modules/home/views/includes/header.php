fb4179185862ae69089d6973fed66d3b2c344fba
shriya01 <jain.shriya@fxbytes.com>
2018-07-26 12:39:32 +0530

    admin and home module changes by shriya

---
+++ application/modules/home/views/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo isset($title) ? $title : 'E-Shop User';?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+
--- application/modules/home/views/loginform.php
+++ application/modules/home/views/loginform.php
@@ -1,40 +1,40 @@
 <section id="content">
-  <div class="container">
-    <div class="row">
-      <?php 
-      if (isset($this->session->loginFailed)) {
-        echo "<div class='text-center text-danger'>".$this->session->loginFailed."</div>";
-      }
-      ?>
-      <?php echo validation_errors('<div class="text-center text-danger"> ', '</div>');?>
-      <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-        <?php 
-        $attributes = array('class' => 'loginform', 'id' => 'myform','role' => 'form');
-        echo form_open('', $attributes);
-        ?>
-        <h2>Sign in</h2>
-        <hr class="colorgraph">
-        <div class="form-group">
-          <?php 
-          $emailOptions = ['type'=>'email','name'=>'email','id'=>'email','placeholder'=>'Email Address','class'=>'form-control  input-lg', 'tabindex'=>'4'];
-//Email Field
-          echo form_label('Email', 'email', ['class'=>'sr-only']);
-          echo form_input($emailOptions); ?>
-        </div>
-        <div class="form-group">
-          <?php 
-//Password Field
-          echo form_label('Password', 'password', ['class'=>'sr-only']);
-          echo form_password('password', '', ['id'=>'password','class'=>'form-control input-lg','placeholder'=>'Password']); ?>
-        </div>
-        <hr class="colorgraph">
+    <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-md-3"><?php echo form_submit('login', 'Login', ['class' => 'btn btn-primary btn-block btn-lg','id'=>'login']); ?>
-          </div>
-        </div>
-        <?php echo form_close(); ?>
-        <a href="<?php echo base_url();?>home/register" class="pull-right">Create Your Account Here</a>
-      </div>
-    </div>
-  </div>
-</section>
+            <?php 
+            if (isset($this->session->loginFailed)) {
+                echo "<div class='text-center text-danger'>".$this->session->loginFailed."</div>";
+            }
+            ?>
+            <?php echo validation_errors('<div class="text-center text-danger"> ', '</div>');?>
+            <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+                <?php 
+                $attributes = array('class' => 'loginform', 'id' => 'myform','role' => 'form');
+                echo form_open('', $attributes);
+                ?>
+
+
+                <div class="form-group">
+                    <?php 
+                    $email = ['name'=> 'email','id' =>'email', 'class'=>'form-control input-lg','maxlength'=> '100',
+                    'placeholder'=>'Email','type'=> 'email','tabindex'=> '3','value'=>$this->input->post('email')?$$this->input->post('email'):''];
+                    echo form_label('Email', 'email', ['class'=>'sr-only']);
+                    echo form_input($email);
+                    ?>
+                </div>
+                <div class="form-group">
+                    <?php 
+//Password Field
+                    echo form_label('Password', 'password', ['class'=>'sr-only']);
+                    echo form_password('password', '', ['id'=>'password','class'=>'form-control input-lg','placeholder'=>'Password']); ?>
+                </div>
+ 
+
+                            <?php echo form_submit('login', 'Login', ['class' => 'col-sm-3 btn btn-primary btn-block btn-sm','id'=>'register']); ?>
+
+                            <?php echo form_close(); ?>
+                            <a href="<?php echo base_url();?>home/login" class="pull-right">Already have a account ? Login here</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
